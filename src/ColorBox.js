81d87f3e16fccc25972a67c6ac98ff1da5bf770f
bhoomij <bhoomijoshi31@gmail.com>
2020-01-07 22:35:34 +0530

    Color-palette style and copy to clipboard change

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
   render() {
     const { background, name } = this.props;
     return (
+      <CopyToClipboard text={background}>
       <div style={{background}} className="ColorBox">
-        <span>{name}</span>
-        <span>More</span>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
+      </CopyToClipboard>
     )
   }
 }
